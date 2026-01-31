1e5d74b27eb78f83838830f56b8f0bc3d22a4ef6
ThomasLouarn <tlouarn@systra.info>
2026-01-31 16:39:38 +0100

    css adjustments

--- gourmet/src/components/RecipeCard.tsx
+++ gourmet/src/components/RecipeCard.tsx
@@ -8,22 +8,26 @@ interface RecipeCardProps {
 
 function RecipeCard({ recipe }: RecipeCardProps) {
   return (
-    <div className="col">
-      <div className="card">
+    <div className="col d-flex">
+      <div className="card  w-100">
         {/* Image */}
-        <img
-          src={recipe.image_url}
-          className="card-img-top"
-          alt={recipe.name}
-        />
+        <Link to={`/recettes/${recipe.id}`}>
+          <img
+            src={recipe.image_url}
+            className="card-img-top"
+            alt={recipe.name}
+          />
+        </Link>
 
-        {/* Title, desc and link */}
-        <div className="card-body">
+        {/* Title and descrption */}
+        <div className="card-body d-flex flex-column">
           <h5 className="card-title">{recipe.name}</h5>
           <p className="card-text">{recipe.description}</p>
+
+          {/* Link to the recipe */}
           <Link
             to={`/recettes/${recipe.id}`}
-            className="btn btn-primary cuisiner-button"
+            className="btn btn-primary mt-auto cuisiner-button"
           >
             Cuisiner !
           </Link>
